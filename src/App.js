b186580f41ca992453914871dc1eaf88838a5fe2
Intisar Sikder <sikder.intisar@gmail.com>
2023-06-07 16:57:00 +0100

    added login modal

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
+// import { useState } from 'react';
+// import './App.css';
+// import LogInModal from './components/LogInModal';
+// import PlayContainer from './containers/PlayContainer';
+// import PokemonList from './components/PokemonList';
+
+// function App() {
+//   const [listOfUsers, setListOfUsers] = useState([]);
+//   const [loggedInUserId, setLoggedInUserId] = useState(0);
+
+
+
+//   return (
+//     <>
+//     <h1>Higher or Lower: Pokèmon edition</h1>
+//     <button onClick={!openLogInModal}>Login</button>
+//     <button>Play</button>
+//     <button>LeaderBoard</button>
+    
+
+//     <PlayContainer/>
+//     {/* <LogInModal/> */}
+//     {/* <PokemonList/> */}
+//     </>
+//   );
+// }
+
+// export default App;
+
 import { useState } from 'react';
 import './App.css';
 import LogInModal from './components/LogInModal';
@@ -7,19 +36,36 @@ import PokemonList from './components/PokemonList';
 function App() {
   const [listOfUsers, setListOfUsers] = useState([]);
   const [loggedInUserId, setLoggedInUserId] = useState(0);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
+  const toggleLoginModal = () => {
+    setShowLoginModal(!showLoginModal);
+  };
+
+  const handleLogin = (username, password) => {
+    const newUser = {
+      username,
+      password,
+    };
+    setListOfUsers([...listOfUsers, newUser]);
+    setShowLoginModal(false);
+  };
+
   return (
     <>
-    <h1>Higher or Lower: Pokèmon edition</h1>
-    <button>Login</button>
-    <button>Play</button>
-    <button>LeaderBoard</button>
-    
+      <h1>Higher or Lower: Pokèmon edition</h1>
+      <button onClick={toggleLoginModal}>Login</button>
+      <button>Play</button>
+      <button>LeaderBoard</button>
 
-    <PlayContainer/>
-    {/* <LogInModal/> */}
-    {/* <PokemonList/> */}
+      {showLoginModal && (
+        <LogInModal handleLogin={handleLogin} />
+      )}
+
+      <PlayContainer />
     </>
   );
 }
 
 export default App;
+
--- src/components/LogInModal.js
+++ src/components/LogInModal.js
@@ -1,12 +1,67 @@
-import LogInForm from "./LogInForm";
+// import LogInForm from "./LogInForm";
 
-const LogInModal = () => {
-    return ( 
-        <>
-        <h2>LogInForm</h2>
-        {/* <LogInForm/> */}
-        </>
-     );
-}
+// const LogInModal = () => {
+
+//     const [openLogInModal, setOpenLogInModal] = useState(false);
+//     const [openSignUpModal, setOpenSignUpModal] = useState(false);
+
+//     const handleToggleLogInModal = () => setOpenLogInModal(!openLogInModal);
+//     const handleToggleSignUpModal = () => setOpenSignUpModal(!openSignUpModal);
+
+//     return ( 
+//         <>
+//         {/* <LogInForm/> */}
+//         </>
+//      );
+// }
  
+// export default LogInModal;
+
+import { useState } from "react";
+
+const LogInModal = ({ handleLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (username) => {
+    setUsername(username.target.value);
+  };
+
+  const handlePasswordChange = (password) => {
+    setPassword(password.target.value);
+  };
+
+  const handleSubmit = (submit) => {
+    submit.preventDefault();
+    handleLogin(username, password);
+  };
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </label>
+          <button type="submit">Join</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
 export default LogInModal;
