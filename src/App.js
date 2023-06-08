5c2cdfbbb4680a0e2ce453d6408da0e1735a76f6
Alex Levendis-Takakis <alexlt3001@gmail.com>
2023-06-08 14:45:05 +0100

    adds endgamemodal content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import PlayContainer from './containers/PlayContainer';
 import LogInForm from './components/LogInForm';
+import EndGameModal from './components/EndGameModal';
 
 function App() {
 
   return (
     <>
-      {/* <LogInForm/> */}
+      <LogInForm/>
       <PlayContainer />
+      <EndGameModal/>
     </>
   );
 }
--- src/components/EndGameModal.js
+++ src/components/EndGameModal.js
@@ -1,12 +1,15 @@
 import App from "../App";
 import PlayContainer from "../containers/PlayContainer";
+import score from "../containers/PlayContainer"
 
 const EndGameModal = () => {
     return ( 
         <>
-        {/* <App/> */}
-        {/* <PlayContainer/> */}
-        <h2>EndGameModal</h2>
+            {/* <App/> */}
+            {/* <PlayContainer/> */}
+            <h1>Game Over!</h1>
+            <p>Final Score: {score}</p>
+            <button>Play again</button>
         </>
      );
 }
