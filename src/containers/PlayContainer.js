db452c854d29476e66e0d8d8e83b1b6974701ce3
Intisar Sikder <sikder.intisar@gmail.com>
2023-06-12 12:41:49 +0100

    fixed pokemon logic

--- src/containers/PlayContainer.js
+++ src/containers/PlayContainer.js
@@ -77,59 +77,165 @@
 // };
 // export default PlayContainer;
 
+
+// import React, { useEffect, useState } from "react";
+
+// const PlayContainer = () => {
+//   const [score, setScore] = useState(0);
+//   const [lives, setLives] = useState(3);
+//   const [highScore, setHighScore] = useState(0);
+//   const [pokemons, setPokemons] = useState([]);
+
+//   useEffect(() => {
+//     const fetchPokemons = async () => {
+//       try {
+//         const response1 = await fetch("http://localhost:8080/api/pokemon/random");
+//         const response2 = await fetch("http://localhost:8080/api/pokemon/random");
+//         const data1 = await response1.json();
+//         const data2 = await response2.json();
+
+//         setPokemons([data1, data2]);
+//       } catch (error) {
+//         console.error("Error fetching Pokemon data:", error);
+//       }
+//     };
+
+//     fetchPokemons();
+//   }, []);
+
+//   return (
+//     <>
+//       <h1>Hello Container</h1>
+//       {pokemons.map((pokemon, index) => (
+//         <div key={index}>
+//           <h2>{pokemon.name}</h2>
+//           <p>Total Base Stat: {pokemon.totalBaseStat}</p>
+//           <img src={pokemon.imageUrl} alt={pokemon.name} />
+//         </div>
+//       ))}
+//     </>
+//   );
+// };
+
+// export default PlayContainer;
+
+
+// import React, { useEffect, useState } from "react";
+// import { useNavigate } from 'react-router-dom';
+
+// const PlayContainer = () => {
+//   const [score, setScore] = useState(0);
+//   const [lives, setLives] = useState(3);
+//   const [highScore, setHighScore] = useState(0);
+//   const [pokemons, setPokemons] = useState([]);
+
+//     const navigate = useNavigate();
+
+//   const fetchPokemons = async () => {
+//     const response1 = await fetch(`http://localhost:8080/api/pokemon/random`);
+//     const response2 = await fetch(`http://localhost:8080/api/pokemon/random`);
+//     const data1 = await response1.json();
+//     const data2 = await response2.json();
+
+//     setPokemons([data1, data2]);
+// };
+
+//   useEffect(() => {
+//     fetchPokemons();
+//   }, []);
+
+//   const handleAnswer = () => {
+//     if(pokemons.totalBaseStat = pokemons.totalBaseStat){
+//         fetchPokemons()
+//     }
+//     else if(pokemons[1].totalBaseStat > pokemons[2].totalBaseStat || pokemons[2].totalBaseStat > pokemons[1].totalBaseStat){
+//         setScore(score+1);
+//     }
+//     else if(lives > 1 && pokemons[1].totalBaseStat < pokemons[2].totalBaseStat || pokemons[2].totalBaseStat < pokemons[1].totalBaseStat){
+//         setLives(lives-1);
+//     }
+//     else if(lives = 1 && (pokemons[1].totalBaseStat < pokemons[2].totalBaseStat || pokemons[2].totalBaseStat < pokemons[1].totalBaseStat)){
+//         if(score > highScore){
+//             setHighScore(score);
+//         }
+//         navigate("/endgame")
+//     }
+//   }
+
+//   return (
+//     <>
+//         <h1>Hello Container</h1>
+//         <p>Score: {score}</p>
+//         <p>Lives: {lives}</p>
+//         <p>High Score: {highScore}</p>
+//       {pokemons.map((pokemon, index) => (
+//         <div key={index}>
+//           <h2>{pokemon.name}</h2>
+//           <p>Total Base Stat: {pokemon.totalBaseStat}</p>
+//           <img src={pokemon.imageUrl} onClick={handleAnswer}/>
+//         </div>
+//       ))}
+//     </>
+//   );
+// };
+
+// export default PlayContainer;
+
+
 import React, { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const PlayContainer = () => {
   const [score, setScore] = useState(0);
   const [lives, setLives] = useState(3);
   const [highScore, setHighScore] = useState(0);
   const [pokemons, setPokemons] = useState([]);
-
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const fetchPokemons = async () => {
-    const response1 = await fetch(`http://localhost:8080/api/pokemon/random`);
-    const response2 = await fetch(`http://localhost:8080/api/pokemon/random`);
+    const response1 = await fetch("http://localhost:8080/api/pokemon/random");
+    const response2 = await fetch("http://localhost:8080/api/pokemon/random");
     const data1 = await response1.json();
     const data2 = await response2.json();
 
     setPokemons([data1, data2]);
-};
+  };
 
   useEffect(() => {
     fetchPokemons();
   }, []);
 
-  const handleAnswer = () => {
-    if(pokemons.totalBaseStat = pokemons.totalBaseStat){
-        fetchPokemons()
+  const handleAnswer = (selectedPokemon) => {
+    const selectedPokemonStats = selectedPokemon === 0 ? pokemons[0].totalBaseStat : pokemons[1].totalBaseStat;
+    const otherPokemonStats = selectedPokemon === 0 ? pokemons[1].totalBaseStat : pokemons[0].totalBaseStat;
+
+    if (selectedPokemonStats > otherPokemonStats) {
+      setScore(score + 1);
+    } else {
+      setLives(lives - 1);
     }
-    else if(pokemons[1].totalBaseStat > pokemons[2].totalBaseStat || pokemons[2].totalBaseStat > pokemons[1].totalBaseStat){
-        setScore(score+1);
+  };
+
+  useEffect(() => {
+    if (lives === 0) {
+      if (score > highScore) {
+        setHighScore(score);
+      }
+      navigate("/endgame", { state: { score } });
     }
-    else if(lives > 1 && pokemons[1].totalBaseStat < pokemons[2].totalBaseStat || pokemons[2].totalBaseStat < pokemons[1].totalBaseStat){
-        setLives(lives-1);
-    }
-    else if(lives = 1 && (pokemons[1].totalBaseStat < pokemons[2].totalBaseStat || pokemons[2].totalBaseStat < pokemons[1].totalBaseStat)){
-        if(score > highScore){
-            setHighScore(score);
-        }
-        navigate("/endgame")
-    }
-  }
+  }, [lives, score, highScore, navigate]);
 
   return (
     <>
-        <h1>Hello Container</h1>
-        <p>Score: {score}</p>
-        <p>Lives: {lives}</p>
-        <p>High Score: {highScore}</p>
+      <h1>Hello Container</h1>
+      <p>Score: {score}</p>
+      <p>Lives: {lives}</p>
+      <p>High Score: {highScore}</p>
       {pokemons.map((pokemon, index) => (
         <div key={index}>
           <h2>{pokemon.name}</h2>
           <p>Total Base Stat: {pokemon.totalBaseStat}</p>
-          <img src={pokemon.imageUrl} onClick={handleAnswer}/>
+          <img src={pokemon.imageUrl} onClick={() => handleAnswer(index)} />
         </div>
       ))}
     </>
@@ -137,3 +243,4 @@ const PlayContainer = () => {
 };
 
 export default PlayContainer;
+
