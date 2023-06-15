acf00d5015a63b8f6c508808f8c6f40af0dc6ba7
Umar Twynam <umartwynam@gmail.com>
2023-06-15 14:56:46 +0100

    PlayContainer hearts for lives, and base stats show on click

--- src/containers/PlayContainer.js
+++ src/containers/PlayContainer.js
@@ -1,20 +1,117 @@
+// import React, { useContext, useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { Filecontext } from "../reactrouter/FileContext";
+// import './Play.css';
+
+// const PlayContainer = () => {
+//     const [score, setScore] = useState(0);
+//     const [lives, setLives] = useState(3);
+//     const [highScore, setHighScore] = useState(() => {
+//     //   const storedHighScore = localStorage.getItem("highScore");
+//     //   return storedHighScore 
+//     });
+//     const [pokemons, setPokemons] = useState([]);
+
+//     const {currentGame, setCurrentGame} = useContext(Filecontext);
+
+//     const navigate = useNavigate();
+
+//   const fetchPokemons = async () => {
+//     const response1 = await fetch("http://localhost:8080/api/pokemon/random");
+//     const response2 = await fetch("http://localhost:8080/api/pokemon/random");
+//     const data1 = await response1.json();
+//     const data2 = await response2.json();
+
+//     const capitalizedData1 = {
+//       ...data1,
+//       name: capitalizeFirstLetter(data1.name)
+//     };
+  
+//     const capitalizedData2 = {
+//       ...data2,
+//       name: capitalizeFirstLetter(data2.name)
+//     };
+  
+//     setPokemons([capitalizedData1, capitalizedData2]);
+//   };
+  
+//   const capitalizeFirstLetter = (string) => {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+//   };
+
+//   useEffect(() => {
+//     fetchPokemons();
+//   }, []);
+
+//   const handleAnswer = (selectedPokemon) => {
+//     const selectedPokemonStats = selectedPokemon === 0 ? pokemons[0].totalBaseStat : pokemons[1].totalBaseStat;
+//     const otherPokemonStats = selectedPokemon === 0 ? pokemons[1].totalBaseStat : pokemons[0].totalBaseStat;
+
+//     if (selectedPokemonStats > otherPokemonStats) {
+//       setScore(score + 1);
+//       fetchPokemons();
+//     } else if (selectedPokemonStats < otherPokemonStats){
+//       setLives(lives - 1);
+//       fetchPokemons();
+//     } else if (selectedPokemonStats === otherPokemonStats){
+//         fetchPokemons();
+//     }
+//   };
+//   const endTheGame = async () => {
+//     console.log(score);
+  
+//     const requestBody = JSON.stringify({
+//       score: score,
+//       isComplete: true
+//     });
+  
+//     const response = await fetch(`http://localhost:8080/games/${currentGame.id}/score`, {
+//       method: 'PATCH',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: requestBody
+//     });
+  
+//     const finishedGame = await response.json();
+//     setCurrentGame(finishedGame);
+//     console.log(finishedGame);
+  
+//     navigate('/endgame', {state:{score}});
+//   };
+
+//   return (
+//     <>
+//       <div className="container-header">
+//       <p>Score: {score}</p>
+//       <p>Lives: {lives}</p>
+//       </div>
+//       <div className="pokemon-container">
+//       {pokemons.map((pokemon, index) => (
+//         <div key={index}>
+//           <h2>{pokemon.name}</h2>
+//           {/* <p>Total Base Stat: {pokemon.totalBaseStat}</p> */}
+//           <img src={pokemon.imageUrl} className="pokemon-image" onClick={() => handleAnswer(index)} />
+//         </div>
+//       ))}
+//       </div>
+//     </>
+//     )
+
+// export default PlayContainer;
+
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Filecontext } from "../reactrouter/FileContext";
 import './Play.css';
 
 const PlayContainer = () => {
-    const [score, setScore] = useState(0);
-    const [lives, setLives] = useState(3);
-    const [highScore, setHighScore] = useState(() => {
-    //   const storedHighScore = localStorage.getItem("highScore");
-    //   return storedHighScore 
-    });
-    const [pokemons, setPokemons] = useState([]);
+  const [score, setScore] = useState(0);
+  const [lives, setLives] = useState(3);
+  const [highScore, setHighScore] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
 
-    const {currentGame, setCurrentGame} = useContext(Filecontext);
+  const { currentGame, setCurrentGame } = useContext(Filecontext);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const fetchPokemons = async () => {
     const response1 = await fetch("http://localhost:8080/api/pokemon/random");
@@ -24,17 +121,19 @@ const PlayContainer = () => {
 
     const capitalizedData1 = {
       ...data1,
-      name: capitalizeFirstLetter(data1.name)
+      name: capitalizeFirstLetter(data1.name),
+      showStats: false
     };
-  
+
     const capitalizedData2 = {
       ...data2,
-      name: capitalizeFirstLetter(data2.name)
+      name: capitalizeFirstLetter(data2.name),
+      showStats: false
     };
-  
+
     setPokemons([capitalizedData1, capitalizedData2]);
   };
-  
+
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
@@ -46,93 +145,99 @@ const PlayContainer = () => {
   const handleAnswer = (selectedPokemon) => {
     const selectedPokemonStats = selectedPokemon === 0 ? pokemons[0].totalBaseStat : pokemons[1].totalBaseStat;
     const otherPokemonStats = selectedPokemon === 0 ? pokemons[1].totalBaseStat : pokemons[0].totalBaseStat;
-
+  
+    setPokemons(prevPokemons => prevPokemons.map((pokemon, index) => {
+      if (index === selectedPokemon) {
+        return { ...pokemon, showStats: true };
+      }
+      return { ...pokemon, showStats: true, statsTimeout: true };
+    }));
+  
     if (selectedPokemonStats > otherPokemonStats) {
       setScore(score + 1);
-      fetchPokemons();
-    } else if (selectedPokemonStats < otherPokemonStats){
+    } else if (selectedPokemonStats < otherPokemonStats) {
       setLives(lives - 1);
-      fetchPokemons();
-    } else if (selectedPokemonStats === otherPokemonStats){
-        fetchPokemons();
     }
-  };
-  const endTheGame = async () => {
-    console.log(score);
   
-    const requestBody = JSON.stringify({
-      score: score,
-      isComplete: true
-    });
-  
-    const response = await fetch(`http://localhost:8080/games/${currentGame.id}/score`, {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: requestBody
-    });
-  
-    const finishedGame = await response.json();
-    setCurrentGame(finishedGame);
-    console.log(finishedGame);
-  
-    navigate('/endgame', {state:{score}});
+    setTimeout(() => {
+      setPokemons(prevPokemons => prevPokemons.map((pokemon, index) => {
+        if (index === selectedPokemon) {
+          return { ...pokemon, showStats: true };
+        }
+        return { ...pokemon, showStats: false, statsTimeout: false };
+      }));
+      fetchPokemons();
+    }, 3000);
   };
   
+
   useEffect(() => {
     if (lives === 0) {
       if (score > highScore) {
         setHighScore(score);
         // localStorage.setItem("highScore", score.toString());
       }
-      endTheGame()
+      endTheGame();
     }
-  }, [lives, score, highScore, navigate]);
+  }, [lives, score, highScore]);
+
+  const endTheGame = async () => {
+    console.log(score);
+
+    const requestBody = JSON.stringify({
+      score: score,
+      isComplete: true
+    });
+
+    const response = await fetch(`http://localhost:8080/games/${currentGame.id}/score`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: requestBody
+    });
+
+    const finishedGame = await response.json();
+    setCurrentGame(finishedGame);
+    console.log(finishedGame);
+
+    navigate('/endgame', { state: { score } });
+  };
 
   return (
     <>
       <div className="container-header">
-      <p>Score: {score}</p>
-      <p>Lives: {lives}</p>
+        <p>Score: {score}</p>
+        <div className="hearts-container">
+          {Array.from({ length: lives }, (_, index) => (
+            <i className="fas fa-heart" key={index}></i>
+          ))}
+        </div>
       </div>
       <div className="pokemon-container">
-      {pokemons.map((pokemon, index) => (
-        <div key={index}>
-          <h2>{pokemon.name}</h2>
-          {/* <p>Total Base Stat: {pokemon.totalBaseStat}</p> */}
-          <img src={pokemon.imageUrl} className="pokemon-image" onClick={() => handleAnswer(index)} />
-        </div>
-      ))}
+        {pokemons.length > 0 ? (
+          pokemons.map((pokemon, index) => (
+            <div key={index}>
+              <h2>{pokemon.name}</h2>
+              {pokemon.showStats ? (
+                <p>Total Base Stat: {pokemon.totalBaseStat}</p>
+              ) : (
+                <img
+                  src={pokemon.imageUrl}
+                  className="pokemon-image"
+                  onClick={() => handleAnswer(index)}
+                  alt={pokemon.name}
+                />
+              )}
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </>
-    )
-      };
-
+  );
+};
 
 export default PlayContainer;
-
-// return (
-//   <>
-//       <div className="container-header">
-//           <p>Score: {score}</p>
-//           <p>Lives: {lives}</p>
-//           <p>High Score: {highScore}</p>
-//       </div>
-//       <h1 className="container-title">Which has the higher total power?</h1>
-//       <div className="pokemon-container">
-//           {pokemons.map((pokemon, index) => (
-//               <div key={index}>
-//                   <h2>{pokemon.name}</h2>
-//                   <img
-//                       src={pokemon.imageUrl}
-//                       className="pokemon-image"
-//                       onClick={() => handleAnswer(index)}
-//                       alt="pokemon"
-//                   />
-//               </div>
-//           ))}
-//       </div>
-//   </>
-// );
 
 
 
